4cd4622e1e08e366d1e69e19ed08c8bb2e8f5fd9
Ian MacLeod <ian@nevir.net>
2012-07-08 21:25:50 -0700

    Start of a service target template

---
+++ SMJobKit Service.xctemplate/main.m
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+int main(int argc, const char *argv[])
+{
+	// NOTE: Before you write any code!
+	//
+	// Open up ___PACKAGENAME___-Info.plist and change the certificate subject name in the authorized
+	// clients entry to EXACTLY match your developer certificate's subject name!
+	//
+	// Aka you're replacing the "Mac Developer: Your Name (##########)" part of the certificate query.
+	
+	return 0;
+}
