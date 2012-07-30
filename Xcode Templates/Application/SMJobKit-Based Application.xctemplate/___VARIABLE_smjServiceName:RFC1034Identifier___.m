6a17873d8de993e3715d7cfc59df598e511696df
Ian MacLeod <ian@nevir.net>
2012-07-29 19:40:13 -0700

    Basic client class

---
+++ Xcode Templates/Application/SMJobKit-Based Application.xctemplate/___VARIABLE_smjServiceName:RFC1034Identifier___.m
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
++ (NSString*) serviceIdentifier
+{
+  return @"___VARIABLE_bundleIdentifierPrefix:bundleIdentifier___.___VARIABLE_productName___.___VARIABLE_smjServiceName:RFC1034Identifier___";
+}
+
+@end
